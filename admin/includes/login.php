1a2de86b77e4ca3d0642609c1b3129e725529333
Brian Fariña <brianfap56@gmail.com>
2022-12-01 16:03:15 +0000

    Login y register

---
+++ admin/includes/login.php
@@ -0,0 +1,28 @@
+<div class="login-box">
+  <h2>Iniciar sesion</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Iniciar sesion
+    </a>
+    <p style="color: white;">¿No tienes cuenta?</p>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Registrarse
+    </a>
+  </form>
+</div>
---
+++ admin/includes/registro.php
@@ -0,0 +1,28 @@
+<div class="login-box">
+  <h2>Registrarse</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Usuario</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Contraseña</label>
+    </div>
+    <div class="user-box">
+      <input type="email" name="" required="">
+      <label>Email</label>
+    </div>
+    <div class="user-box">
+      <input type="checkbox" name="" required="">
+      <label>Activo</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Registrarse
+    </a>
+  </form>
+</div>
