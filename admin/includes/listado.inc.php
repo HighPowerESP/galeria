27030e0f7182c452e9220eb865cec407ac998824
Brian Fariña <brianfap56@gmail.com>
2022-12-04 16:06:32 +0000

    .

--- admin/includes/listado.inc.php
+++ admin/includes/listado.inc.php
@@ -1,14 +1,15 @@
 <?php
+include dirname(dirname(dirname(__FILE__)))."/common/utils.php";
 include dirname(dirname(dirname(__FILE__)))."/common/config.php";
 include dirname(dirname(dirname(__FILE__)))."/common/mysql.php";
 
-$conn = Connect($config['database']);
+$connection = Connect( $config['database']);
 
 $sql = "select * from authors order by name asc";
 
-$rows = ExecuteQuery($sql, $conn);
+$rows = ExecuteQuery($sql, $connection);
 
-$conn->close();
+$connection->close();
 
 ?>
 <script>
